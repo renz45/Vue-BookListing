c99708d285ba1288c012ec8acce94973b560e29a
Jon Friskics <jonfriskics@gmail.com>
2018-04-03 11:06:30 -0400

    server log tweak

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
-console.log("port" + process.env.PORT);
-
 var port = process.env.PORT || 5000;
 app.listen(port);
+
+console.log("port " + port);
