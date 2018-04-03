409177879b0a431af10dee7cbf805fb4a6320ae3
Jon Friskics <jonfriskics@gmail.com>
2018-04-03 11:10:51 -0400

    add history middleware

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 let express = require('express');
 let path = require('path');
 let serveStatic = require('serve-static');
+let history = require('connect-history-api-fallback');
 
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+app.use(history());
 
 var port = process.env.PORT || 5000;
 app.listen(port);
